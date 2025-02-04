87f55c5386f8b1d3f03c22c02f2a022513c2f687
chacha86 <taejincha0619@gmail.com>
2025-02-04 15:38:10 +0900

    duplication g

--- src/main/java/com/example/rest_tdd/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/rest_tdd/domain/member/member/controller/ApiV1MemberController.java
@@ -4,6 +4,7 @@ import com.example.rest_tdd.domain.member.member.dto.MemberDto;
 import com.example.rest_tdd.domain.member.member.entity.Member;
 import com.example.rest_tdd.domain.member.member.service.MemberService;
 import com.example.rest_tdd.global.dto.RsData;
+import com.example.rest_tdd.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,12 @@ public class ApiV1MemberController {
 
     @PostMapping("/join")
     public RsData<MemberDto> join(@RequestBody JoinReqBody reqBody) {
+
+        memberService.findByUsername(reqBody.username())
+                .ifPresent(_ -> {
+                    throw new ServiceException("409-1", "이미 사용중인 아이디입니다.");
+                });
+
 
         Member member = memberService.join(reqBody.username(), reqBody.password(), reqBody.nickname());
         return new RsData<>(
