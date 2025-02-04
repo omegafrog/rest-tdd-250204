614a0ecd58d022a9826ba39ce300ee85eb823b19
chacha86 <taejincha0619@gmail.com>
2025-02-04 12:55:43 +0900

    application.yml

--- src/test/java/com/example/rest_tdd/RestTddApplicationTests.java
+++ src/test/java/com/example/rest_tdd/RestTddApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.rest_tdd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestTddApplicationTests {
 
 	@Test
