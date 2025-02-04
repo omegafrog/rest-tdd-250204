6434f4e196514758996861be40b9447bab8f9a8d
chacha86 <taejincha0619@gmail.com>
2025-02-04 14:20:08 +0900

    join - r

--- src/test/java/com/example/rest_tdd/RestTddApplicationTests.java
+++ src/test/java/com/example/rest_tdd/RestTddApplicationTests.java
@@ -1,15 +1,41 @@
 package com.example.rest_tdd;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
 class RestTddApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("회원 가입")
+	void join() throws Exception {
+
+		ResultActions resultActions = mvc
+				.perform(
+						post("/api/v1/members/join")
+				)
+				.andDo(print());
+
+		resultActions
+				.andExpect(status().isCreated());
+
 	}
 
 }
