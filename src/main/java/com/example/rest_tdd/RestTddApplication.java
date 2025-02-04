ba432731534ad577985d6ca3f5b9f65fbf853b87
chacha86 <taejincha0619@gmail.com>
2025-02-04 12:51:31 +0900

    init

---
+++ src/main/java/com/example/rest_tdd/RestTddApplication.java
@@ -0,0 +1,15 @@
+package com.example.rest_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class RestTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTddApplication.class, args);
+	}
+
+}
